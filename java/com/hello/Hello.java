eb5f8f692d6d1c1bc373325d2c22fe2beec655e0
leal <lijiadongyue@gmail.com>
2019-05-02 21:57:00 +0800

    modify java

--- java/com/hello/Hello.java
+++ java/com/hello/Hello.java
@@ -1,6 +1,10 @@
 package com.hello;
 
 class Hello {
+    /**
+     * 调试java带有package的时候直接使用java +class会有问题
+     * @param args
+     */
     public static void main(String[] args) {
         int i = 1;
         System.out.println(i + 1);
