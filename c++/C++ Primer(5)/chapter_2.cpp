01951dae214e5dba5d8c7596075cc39c520c9833
leal <lijiadongyue@gmail.com>
2019-11-12 01:03:16 +0800

    add until p_36

--- c++/C++ Primer(5)/chapter_2.cpp
+++ c++/C++ Primer(5)/chapter_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <typeinfo>
 using namespace std;
 
 void p_3()
@@ -50,6 +51,78 @@ void p_16()
     cout << "i= " << i << " &i= " << r1 << endl;
     cout << "d= " << d << " &d= " << r2 << endl;
 }
+void p_18()
+{
+    int i = 5, j = 10;
+    // 指针p指向i的地址
+    int *p = &i;
+    cout << "p: " << p << " "
+         << "*p: " << *p << endl;
+    // p 重新指向j的地址，改变之地址
+    p = &j;
+    cout << "p: " << p << " "
+         << "*p: " << *p << endl;
+    // 指向的地址值改变，地址未改变
+    *p = 20;
+    cout << "p: " << p << " "
+         << "*p: " << *p << endl;
+    // p的地址指向j，j改变，同时*p也会改变
+    j = 30;
+    cout << "p: " << p << " "
+         << "*p: " << *p << endl;
+}
+void p_20()
+{
+    int i = 42;
+    int *p = &i;
+    cout << p << " " << *p << endl;
+    *p = *p * *p;
+    cout << p << " " << *p << endl;
+}
+void p_22()
+{
+    int i = 0;
+    int *p = nullptr;
+    int *p1 = &i;
+    if (!p)
+    {
+        cout << "p 指向一个空地址\n";
+    }
+    if (!*p1)
+    {
+        cout << "p 指向的引用值为0\n";
+    }
+}
+void p_34()
+{
+    int i = 0, &r = i;
+    auto a = r;
+    const int ci = i, &cr = ci;
+    auto b = ci;
+    auto c = cr;
+    auto d = &i;
+    auto e = &ci;
+    auto &f = ci;
+    cout << a << " " << b << " " << c << " " << d << " " << e << " " << f << endl;
+    a = 42, b = 42, c = 42;
+    // d e 是指针，f是常量引用
+    //d = 42, e = 42, f = 42;
+    cout << a << " " << b << " " << c << " " << d << " " << e << " " << f << endl;
+}
+void p_35()
+{
+    const int i = 42;
+    auto j = i;
+    const auto &k = i;
+    auto *p = &i;
+    const auto j2 = i, &k2 = i;
+    cout << typeid(i).name() << endl;
+    cout << typeid(j).name() << endl;
+    cout << typeid(k).name() << endl;
+    cout << typeid(p).name() << endl;
+    cout << typeid(j2).name() << endl;
+    cout << typeid(k2).name() << endl;
+}
 void test()
 {
     short a = 1;
@@ -71,8 +144,24 @@ void test()
     cout << sizeof(e) << endl;
     cout << sizeof(f) << endl;
 }
+void p_36()
+{
+    int a = 3, b = 4;
+    // 使用不加括号的的变量，c的类型就是a的类型，等同于int c=a
+    decltype(a) c = a; 
+    // 使用加括号的变量，d的类型为引用，等同于 int &d=a
+    decltype((b)) d = a;
+    ++c, ++d;
+    cout << a << " " << b << " " << c << " " << d << endl;
+}
 int main()
 {
     // p_16();
+    // p_18();
+    // p_20();
+    // p_22();
+    // p_34();
+    // p_35();
+    p_36();
     return 0;
 }
